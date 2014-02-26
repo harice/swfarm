43c1c8b1b968cfbde293ae23afe9b50d43a73a2f
Avs <avs@Avss-MacBook-Pro.local>
2014-02-26 18:17:53 +0800

    update make emp no unique

--- app/repositories/UsersRepository.php
+++ app/repositories/UsersRepository.php
@@ -14,7 +14,7 @@ class UsersRepository implements UsersRepositoryInterface {
     'email' => 'required|email|unique:users',
     'firstname' => 'required',
     'lastname' => 'required',
-    'emp_no' => 'unique'
+    'emp_no' => 'unique:users'
   );
 
   public function findById($id){
