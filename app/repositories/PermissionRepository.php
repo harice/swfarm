0ec9374b3c92e9b78c73257b19fd6d91caf6282a
Avs Ceriola <avelino.ceriola@elementzinteractive.com>
2014-03-06 16:17:23 +0800

    update on role module

--- app/repositories/PermissionRepository.php
+++ app/repositories/PermissionRepository.php
@@ -2,18 +2,6 @@
  
 class PermissionRepository implements PermissionRepositoryInterface {
 
-  public function findAll(){
-    echo "find All here";
-  }
-
-  public function findById($id){
-    
-  }
-
-  public function paginate($perPage, $offset){
-  
-  }
-
   public function store($data){
    $rules = array(
       'role' => 'required',
@@ -23,13 +11,28 @@ class PermissionRepository implements PermissionRepositoryInterface {
    $this->validate($data, $rules);
 
     //client must pass value in comma separated format
-    $permissionIds = explode(',', $data['permissions']); 
+    $permissionIds = explode(',', $data['permissions']);
+
+    //check if permission given exist in the database
+    foreach($permissionIds as $item){
+      if(count(PermissionCategoryType::find($item)) > 0) {
+        continue;
+      }
+      return Response::json(array(
+          'error' => true,
+          'message' => 'One of the permission given does not exist on the database.'),
+          200
+      );
+    }
+    
     //deleting permissions that is uncheck in client side
     if($data['role'] == '' || $data['role'] == null){
       Permission::where('role', '=', $data['role'])->delete(); //deleting all permission to role if client send empty role value
     } else {
+      //deleting the permissions on db if it doesn't exist on the current given permission
       Permission::where('role', '=', $data['role'])->whereNotIn('permissioncategorytype', $permissionIds)->delete(); 
 
+      //Adding the new permissions given
       foreach($permissionIds as $permissionId){
           if(Permission::where('role', '=', $data['role'])->where('permissioncategorytype', '=', $permissionId)->count() > 0){
             continue; //skip adding permission already exist
@@ -53,14 +56,6 @@ class PermissionRepository implements PermissionRepositoryInterface {
       
   }
 
-  public function update($id, $data){
-   
-  }
-
-  public function destroy($id){
-    
-  }
-  
   public function validate($data, $rules){
     $validator = Validator::make($data, $rules);
 
