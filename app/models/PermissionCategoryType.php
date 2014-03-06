606eda7af067c8d1cf726dfef6f77c5d157edf11
Avs Ceriola <avelino.ceriola@elementzinteractive.com>
2014-03-06 15:13:51 +0800

    update on permission module

--- app/models/PermissionCategoryType.php
+++ app/models/PermissionCategoryType.php
@@ -6,11 +6,11 @@ class PermissionCategoryType extends Eloquent {
     protected $table = 'permissioncategorytype';
 
     public function permissionType(){
-    	return $this->hasMany('PermissionType');
+    	return $this->hasMany('PermissionType', 'id', 'permissiontype');
     }
 
     public function permissionCategory(){
-    	return $this->hasMany('PermissionCategory');
+    	return $this->hasMany('PermissionCategory', 'id', 'permissioncategory');
     }
 
     public function permission(){
--- app/repositories/PermissionRepository.php
+++ app/repositories/PermissionRepository.php
@@ -15,7 +15,42 @@ class PermissionRepository implements PermissionRepositoryInterface {
   }
 
   public function store($data){
-   
+   $rules = array(
+      'role' => 'required',
+      'permissions' => 'required'
+    );
+
+   $this->validate($data, $rules);
+
+    //client must pass value in comma separated format
+    $permissionIds = explode(',', $data['permissions']); 
+    //deleting permissions that is uncheck in client side
+    if($data['role'] == '' || $data['role'] == null){
+      Permission::where('role', '=', $data['role'])->delete(); //deleting all permission to role if client send empty role value
+    } else {
+      Permission::where('role', '=', $data['role'])->whereNotIn('permissioncategorytype', $permissionIds)->delete(); 
+
+      foreach($permissionIds as $permissionId){
+          if(Permission::where('role', '=', $data['role'])->where('permissioncategorytype', '=', $permissionId)->count() > 0){
+            continue; //skip adding permission already exist
+          }   
+          $permission = new Permission;
+          $permission->role = $data['role'];
+          $permission->permissioncategorytype = $permissionId;
+
+          $permission->save();
+      }
+
+      $response = Response::json(array(
+          'error' => false,
+          'message' => 'Updated user roles.'),
+          200
+      );
+
+    }
+
+    return $response;
+      
   }
 
   public function update($id, $data){
@@ -38,5 +73,43 @@ class PermissionRepository implements PermissionRepositoryInterface {
   public function instance($data = array())
   {
     return new Permission($data);
+  }
+
+  public function getAllPermissionCategoryType(){
+    $permissionCategoryType = PermissionCategoryType::with('permissionType')->with('permissionCategory')->get();
+    
+    return Response::json(
+        $permissionCategoryType->toArray(),
+        200
+      );
+  }
+
+  public function getPermissionByRoleId($id){
+    $rolesWithPermission = Roles::where('id', '=', $id)->with('permission')->firstOrFail();
+
+    if($rolesWithPermission->count() > 0){
+      $response = Response::json(
+        $rolesWithPermission->toArray(),
+        200
+      );
+    } else {
+      $response = Response::json(array(
+        'error' => true,
+        'message' => "Role not found"),
+        200
+      );
+    }
+
+    return $response;
+
+  }
+
+  public function getAllRoleWithPermission(){
+    $rolesWithPermission = Roles::with('permission')->get();
+
+    return Response::json(
+        $rolesWithPermission->toArray(),
+        200
+      );
   }
 }
