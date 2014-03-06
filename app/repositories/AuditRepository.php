05864be9a937f536696f41fcef88798411803225
Romualdo Dasig <romualdo.dasig@elementzinteractive.com>
2014-03-06 19:02:58 +0800

    Initial refactoring for Audit trail.

--- app/repositories/AuditRepository.php
+++ app/repositories/AuditRepository.php
@@ -1,5 +1,5 @@
 <?php
- 
+
 class AuditRepository implements AuditRepositoryInterface {
 
   public function findById($id){
@@ -21,7 +21,16 @@ class AuditRepository implements AuditRepositoryInterface {
     return $response;
   }
   
-  public function paginate($perPage, $offset, $sortby, $orderby, $type, $data_id){
+  public function paginate($input) {
+    
+    $perPage = array_key_exists('perpage', $input) ? $input['perpage'] : Config::get('constants.GLOBAL_PER_LIST');
+    $page = array_key_exists('page', $input) ? $input['page'] : 1;
+    $sortby = array_key_exists('sortby', $input) ? $input['sortby'] : 'created_at';
+    $orderby = array_key_exists('orderby', $input) ? $input['orderby'] : 'desc';
+    $type = array_key_exists('type', $input) ? $input['type'] : '';
+    $data_id = array_key_exists('data_id', $input) ? $input['data_id'] : '';
+    $offset = $page*$perPage-$perPage;
+    
     $errorMsg = null;
     $sortby = strtolower($sortby);
     $orderby = strtolower($orderby);
@@ -34,23 +43,38 @@ class AuditRepository implements AuditRepositoryInterface {
       $errorMsg = 'Order by category not found(ASC or DESC expected).';
     } else {
       
-      // Get Data
+      // Generate Query depending on given parameters.
       // TO DO: Needs to be refactored
       if(!empty($type)){
         if(!empty($data_id)) {
-          $count = Audit::where('type', $type)->where('data_id', $data_id)->count();
-          $auditList = Audit::where('type', $type)->where('data_id', $data_id)->take($perPage)->offset($offset)->orderBy($sortby, $orderby)->get();
+          $count = Audit::where('type', $type)
+              ->where('data_id', $data_id)
+              ->count();
+          $auditList = Audit::where('type', $type)
+              ->where('data_id', $data_id)
+              ->take($perPage)
+              ->offset($offset)
+              ->orderBy($sortby, $orderby)
+              ->get();
         } else {
-          $count = Audit::where('type', $type)->count();
-          $auditList = Audit::where('type', $type)->take($perPage)->offset($offset)->orderBy($sortby, $orderby)->get();
+          $count = Audit::where('type', $type)
+              ->count();
+          $auditList = Audit::where('type', $type)
+              ->take($perPage)
+              ->offset($offset)
+              ->orderBy($sortby, $orderby)
+              ->get();
         }
       } else {
         $count = Audit::count();
-        $auditList = Audit::take($perPage)->offset($offset)->orderBy($sortby, $orderby)->get();
+        $auditList = Audit::take($perPage)
+            ->offset($offset)
+            ->orderBy($sortby, $orderby)
+            ->get();
       }
       $auditList = $auditList->toArray();
       
-      // Convert JSON value into Array
+      // Convert JSON value to Array
       for ($i=0; $i<count($auditList); $i++) {
         $oldValue = unserialize($auditList[$i]["value"])->toArray();
         $auditList[$i]["value"] = $oldValue;
@@ -71,7 +95,6 @@ class AuditRepository implements AuditRepositoryInterface {
     }
 
     return $response;
-
   }
 
   public function store($data){
