ff7dd2f8e0cd2b528c1a3ad66ade581b95d61983
William Go <william.go@elementzinteractive.com>
2014-03-06 15:54:40 +0800

    role-permission

---
+++ public/js/collections/permission/PermissionCategoryTypeCollection.js
@@ -0,0 +1,79 @@
+define([
+	'backbone',
+	'models/permission/PermissionCategoryTypeModel',
+], function(Backbone, PermissionCategoryTypeModel){
+	var PermissionCategoryTypeCollection = Backbone.Collection.extend({
+		url: '/apiv1/permission',
+		model: PermissionCategoryTypeModel,
+		options: {
+			currentPage: 1,
+			maxItem: 0,
+		},
+		initialize: function(){
+			
+		},
+		
+		getAllModels: function (callback, args) {
+			var thisObj = this;
+			this.setGetAllURL();
+			this.fetch({
+				success: function (collection, response, options) {
+					callback(thisObj, args);
+				},
+				error: function (collection, response, options) {
+					if(typeof response.responseJSON.error == 'undefined')
+						alert(response.responseJSON);
+					else
+						alert(response.responseText);
+				},
+			})
+		},
+		
+		getModels: function (callback, args) {
+			var thisObj = this;
+		
+			this.sync('read', this, {
+				success: function (data, textStatus, jqXHR) {
+					//console.log('success: getModelsPerPage');
+					//console.log(data);
+					//console.log(jqXHR);
+					
+					if(textStatus == 'success') {
+						var roles = data.data;
+						
+						thisObj.reset();
+						
+						_.each(roles, function (role) {
+							thisObj.add(new RoleModel(role));
+						});
+						
+						thisObj.options.maxItem = data.total;
+						
+						var getType = {};
+						if(callback && getType.toString.call(callback) === '[object Function]')
+							callback(thisObj, args);
+					}
+					else
+						alert(jqXHR.statusText);
+				},
+				error:  function (jqXHR, textStatus, errorThrown) {
+					alert(jqXHR.statusText);
+				},
+			});
+		},
+		
+		getDefaultURL: function () {
+			return '/apiv1/permission';
+		},
+		
+		setDefaultURL: function () {
+			this.url = this.getDefaultURL();
+		},
+		
+		setGetAllURL: function () {
+			this.url = this.getDefaultURL()+'/getAllPermissionCategoryType';
+		},
+	});
+
+	return PermissionCategoryTypeCollection;
+});
--- public/js/views/permission/PermissionEditView.js
+++ public/js/views/permission/PermissionEditView.js
@@ -1,19 +1,24 @@
 define([
 	'backbone',
+	'collections/permission/PermissionCategoryTypeCollection',
 	'text!templates/layout/contentTemplate.html',
 	'text!templates/permission/permissionListTemplate.html',
 	'constant',
-], function(Backbone, contentTemplate, permissionListTemplate, Const){
+], function(Backbone, PermissionCategoryTypeCollection, contentTemplate, permissionListTemplate, Const){
 
 	var PermissionListView = Backbone.View.extend({
 		el: $("#"+Const.CONTAINER.MAIN),
 		
 		initialize: function() {
-			
+			this.collection = new PermissionCategoryTypeCollection();
+			this.module
 		},
 		
 		render: function(){
+			var thisObj = this;
+		
 			this.displayPermission();
+			this.collection.getAllModels(this.displayPermissionCategoryType, thisObj);
 		},
 		
 		displayPermission: function () {
@@ -26,6 +31,16 @@ define([
 			var compiledTemplate = _.template(contentTemplate, variables);
 			this.$el.html(compiledTemplate);
 		},
+		
+		displayPermissionCategoryType: function (permissionCategoryTypeCollection, thisObj) {
+			console.log(permissionCategoryTypeCollection);
+			thisObj.test();
+		},
+		
+		test: function () {
+			console.log('test123');
+		},
+		
 	});
 
   return PermissionListView;
