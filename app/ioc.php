5d4667dc3d57f230beca9fb28b816f83e6dd35a3
Romualdo Dasig <romualdo.dasig@elementzinteractive.com>
2014-03-10 18:08:18 +0800

    Log changes on permission to audit trail.

--- app/ioc.php
+++ app/ioc.php
@@ -10,13 +10,6 @@
 */
 
 App::bind('UsersRepositoryInterface', 'UsersRepository');
-
 App::bind('RolesRepositoryInterface', 'RolesRepository');
-
 App::bind('PermissionRepositoryInterface', 'PermissionRepository');
-
-App::bind('ProductsRepositoryInterface', 'ProductsRepository');
-
-App::bind('WatchdogRepositoryInterface', 'WatchdogRepository');
-
 App::bind('AuditRepositoryInterface', 'AuditRepository');
