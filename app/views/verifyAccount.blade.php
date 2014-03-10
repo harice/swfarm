1125a1662922976b6745ea64342f9a35f6beb1da
Avs Ceriola <avelino.ceriola@elementzinteractive.com>
2014-03-10 11:29:00 +0800

    update on email verification

---
+++ app/views/verifyAccount.blade.php
@@ -0,0 +1,12 @@
+<html>
+<head>
+	<title>Southwest Farm - Verification of Account</title>
+</head>
+    <body>
+        <div>Hello {{$firstname}},</div>
+        <div>&nbsp;</div>
+        <div>{{$message}}</div>
+        <div>&nbsp;</div>
+        <div>Southwest Farm Adminstrator</div>
+    </body>
+</html>
