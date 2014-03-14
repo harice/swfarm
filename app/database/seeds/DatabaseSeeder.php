0cbfecbdef1d618cbd710c3268cc6e56146556d3
Romualdo Dasig <romualdo.dasig@elementzinteractive.com>
2014-03-14 17:02:50 +0800

    Add 'address_states' and 'address_cities' tables and seeds.

--- app/database/seeds/DatabaseSeeder.php
+++ app/database/seeds/DatabaseSeeder.php
@@ -18,6 +18,9 @@ class DatabaseSeeder extends Seeder {
         // $this->call('ProductsTableSeeder');
 		$this->call('AccountTypeTableSeeder');
 		$this->call('AddressTypeTableSeeder');
+        
+         $this->call('AddressStatesTableSeeder');
+         $this->call('AddressCitiesTableSeeder');
 
 	}
 
