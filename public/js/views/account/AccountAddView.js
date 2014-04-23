fdd18260b3ba20a4018f5f6cbda32db52217d0e4
Romualdo Dasig <romualdo.dasig@elementzinteractive.com>
2014-04-23 15:37:46 +0800

    Disable checking on zipcode

--- public/js/views/account/AccountAddView.js
+++ public/js/views/account/AccountAddView.js
@@ -243,7 +243,7 @@ define([
 					$('.'+addressFieldClassRequired[i]).each(function() {
 						$(this).rules('add', {
 							required: true,
-							valid_zipcode: true,
+							// valid_zipcode: true,
 							messages: {
 								number: 'Please enter a valid zip code.'
 							},
