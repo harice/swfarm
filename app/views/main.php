04e6f0694fe624be8c9e6e020244cfb552db5976
dasigr <romualdo.dasig@elementzinteractive.com>
2014-04-29 10:42:39 +0800

    Changed to blue template.

--- app/views/main.php
+++ app/views/main.php
@@ -18,8 +18,8 @@
 		<link rel="stylesheet" href="fonts/font-awesome-4/css/font-awesome.min.css">
 		<link rel="stylesheet" type="text/css" href="js/libs/jquery.nanoscroller/nanoscroller.css" />
         <link rel="stylesheet" type="text/css" href="js/libs/jquery.select2/select2.css" />
-        <link rel="stylesheet" type="text/css" href="js/libs/jquery.icheck/skins/flat/green.css" />
-	    <link rel="stylesheet" href="css/skin-green.css">
+        <link rel="stylesheet" type="text/css" href="js/libs/jquery.icheck/skins/flat/blue.css" />
+	    <link rel="stylesheet" href="css/skin-blue.css">
         <link rel="stylesheet" href="css/custom.css">
 
 	  	<!--[if lt IE 9]>
