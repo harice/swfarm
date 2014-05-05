cae8a365599091f4bfaa84847b07f47683885d7f
dasigr <romualdo.dasig@elementzinteractive.com>
2014-05-05 16:19:28 +0800

    Add Scale CRUD

---
+++ app/models/Scale.php
@@ -0,0 +1,34 @@
+<?php
+
+/**
+ * Description of FarmLocation
+ *
+ * @author Das
+ */
+class Scale extends BaseModel {
+    
+    /**
+	 * The database table used by the model.
+	 *
+	 * @var string
+	 */
+	protected $table = 'scale';
+    
+    /**
+     * Define fillable attributes in a model.
+     * 
+     * @var array
+     */
+    protected $fillable = array('account_id', 'name');
+
+    /**
+     * Define field validation rules.
+     * 
+     * @var array
+     */
+	public static $rules = array(
+        'account_id' => 'required',
+        'name' => 'required'
+    );
+    
+}
---
+++ app/repositories/ScaleRepository.php
@@ -0,0 +1,151 @@
+<?php
+
+/**
+ * Description of ScaleRepository
+ *
+ * @author Das
+ */
+class ScaleRepository implements ScaleRepositoryInterface {
+    
+    public function findAll($params)
+    {
+        try
+        {
+            $perPage = isset($params['perpage']) ? $params['perpage'] : 10;
+            
+            $result = Scale::paginate($perPage);
+            return $result->getCollection();
+        }
+        catch (Exception $e)
+        {
+            return $e->getMessage();
+        }
+    }
+    
+    public function findById($id)
+    {
+        try
+        {
+            $scale = Scale::find($id);
+            
+            if (!$scale) {
+                throw new NotFoundException();
+            }
+            
+            return $scale;
+        }
+        catch (Exception $e)
+        {
+            return $e->getMessage();
+        }
+    }
+    
+    public function store($data)
+    {
+        try
+        {
+            $this->validate($data);
+            $scale = $this->instance();
+            $scale->fill($data);
+            
+            if (!$scale->save()) {
+                return array(
+                    'error' => true,
+                    'message' => 'Scale was not created.'
+                );
+            }
+            
+            $response = array(
+                'error' => false,
+                'message' => Lang::get('messages.success.created', array('entity' => 'Scale')),
+                'data' => $scale->toArray()
+            );
+            
+            return $response;
+        }
+        catch (Exception $e)
+        {
+            return $e->getMessage();
+        }
+    }
+    
+    public function update($id, $data)
+    {
+        try
+        {
+            $this->validate($data, $id);
+            $scale = $this->findById($id);
+            $scale->fill($data);
+            
+            if (!$scale->update()) {
+                return array(
+                    'error' => true,
+                    'message' => 'Scale was not updated.'
+                );
+            }
+            
+            $response = array(
+                'error' => false,
+                'message' => Lang::get('messages.success.updated', array('entity' => 'Scale')),
+                'data' => $scale->toArray()
+            );
+            
+            return $response;
+        }
+        catch (Exception $e)
+        {
+            return $e->getMessage();
+        }
+    }
+    
+    public function destroy($id)
+    {
+        try
+        {
+            $scale = $this->findById($id);
+
+            if (!$scale->delete()) {
+                return array(
+                    'error' => true,
+                    'message' => 'Scale was not deleted.'
+                );
+            }
+
+            $response = array(
+                'error' => false,
+                'message' => Lang::get('messages.success.deleted', array('entity' => 'Scale')),
+                'data' => $scale->toArray()
+            );
+            
+            return $response;
+        }
+        catch (Exception $e)
+        {
+            return $e->getMessage();
+        }
+    }
+    
+    public function validate($data, $id = null)
+    {
+        $rules = Scale::$rules;
+        
+        if ($id) {
+            $rules['account_id'] = 'required';
+            $rules['name'] = 'required';
+        }
+        
+        $validator = Validator::make($data, $rules);
+        
+        if ($validator->fails()) {
+            throw new ValidationException($validator);
+        }
+        
+        return true;
+    }
+    
+    public function instance($data = array())
+    {
+        return new Scale($data);
+    }
+    
+}
