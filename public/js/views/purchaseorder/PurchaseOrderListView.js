226a76438d29c9505fca8223ea6090703ae86e5c
William Go <william.go@elementzinteractive.com>
2014-05-06 17:28:04 +0800

    fixes

--- public/js/views/purchaseorder/PurchaseOrderListView.js
+++ public/js/views/purchaseorder/PurchaseOrderListView.js
@@ -38,8 +38,10 @@ define([
 			this.collection.on('sync', function() {
 				_.each(this.models, function (model) {
 					model.set('created_at', thisObj.convertDateFormat(model.get('created_at').split(' ')[0], 'yyyy-mm-dd', thisObj.dateFormat, '-'));
-					model.set('transportdatestart', thisObj.convertDateFormat(model.get('transportdatestart').split(' ')[0], 'yyyy-mm-dd', thisObj.dateFormat, '-'));
-					model.set('transportdateend', thisObj.convertDateFormat(model.get('transportdateend').split(' ')[0], 'yyyy-mm-dd', thisObj.dateFormat, '-'));
+					if(model.get('transportdatestart'))
+						model.set('transportdatestart', thisObj.convertDateFormat(model.get('transportdatestart').split(' ')[0], 'yyyy-mm-dd', thisObj.dateFormat, '-'));
+					if(model.get('transportdateend'))
+						model.set('transportdateend', thisObj.convertDateFormat(model.get('transportdateend').split(' ')[0], 'yyyy-mm-dd', thisObj.dateFormat, '-'));
 				});
 				
 				thisObj.displayList();
