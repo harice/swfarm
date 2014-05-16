4e5b3868754574bd299567eab407f3bef1fc38c7
Avs Ceriola <avelino.ceriola@elementzinteractive.com>
2014-05-16 12:05:23 +0800

    added rate column to trailer

--- app/database/migrations/2014_04_21_031445_create_trailer_table.php
+++ app/database/migrations/2014_04_21_031445_create_trailer_table.php
@@ -18,6 +18,7 @@ class CreateTrailerTable extends Migration {
             $table->increments('id');
 			$table->integer('account_id')->unsigned();
             $table->string('number', 20);
+            $table->decimal('rate', 8, 2);
 			$table->timestamps();
             
             $table->foreign('account_id')->references('id')->on('account');
