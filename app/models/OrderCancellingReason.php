10fe5afc4322861679d9178564b7b863f75a3c47
Avs Ceriola <avelino.ceriola@elementzinteractive.com>
2014-06-02 14:44:56 +0800

    PO/SO reason for cancelling

---
+++ app/models/OrderCancellingReason.php
@@ -0,0 +1,41 @@
+<?php
+
+class OrderCancellingReason extends BaseModel {
+    
+    /**
+	 * The database table used by the model.
+	 *
+	 * @var string
+	 */
+	protected $table = 'ordercancellingreason';
+	public $timestamps = false;
+	/**
+     * Define fillable attributes in a model.
+     * 
+     * @var array
+     */
+    protected $fillable = array(
+        'order',
+        'reason',
+        'others'
+    );
+    
+    /**
+     * Define field validation rules.
+     * 
+     * @var array
+     */
+	public static $rules = array(
+        'order' => 'required',
+        'reason' => 'required'
+    );
+
+    public function order(){
+        return $this->hasOne('Order', 'id','order');
+    }
+
+    public function reason(){
+        return $this->hasOne('reason', 'id','reason');
+    }
+
+}
