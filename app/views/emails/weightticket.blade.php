3f4513e4dba76015c335d7675ff321134116d6c7
dasigr <romualdo.dasig@elementzinteractive.com>
2014-07-10 17:18:01 +0800

    fix

--- app/views/emails/weightticket.blade.php
+++ app/views/emails/weightticket.blade.php
@@ -596,7 +596,7 @@
                                     </table>
                                     @endif
                                     
-                                    @if (isset($weightticket["weightticketscale_pickup"]))
+                                    @if (isset($weightticket["weightticketscale_dropoff"]))
                                     <table border="0" cellpadding="0" cellspacing="0" width="100%" id="templateBody">
                                         <tr>
                                             <td valign="top" class="bodyContent" mc:edit="body_content03">
