6df4b79b40b483a01711b86a7fa6d17bd850eb80
Avs Ceriola <avelino.ceriola@elementzinteractive.com>
2014-07-15 14:33:09 +0800

    fix bug

---
+++ app/models/StackLocation.php
@@ -0,0 +1,37 @@
+<?php
+
+class StackLocation extends Eloquent {
+    
+    /**
+	 * The database table used by the model.
+	 *
+	 * @var string
+	 */
+    protected $table = 'stacklocation';
+    
+    /**
+     * Define fillable attributes in a model.
+     * 
+     * @var array
+     */
+    protected $fillable = array(
+		'stack_id',
+        'section_id',
+        'tons'
+    );
+    
+    public static $rules = array(
+		'stack_id' => 'required',
+        'section_id' => 'required',
+        'tons' => 'required'
+    );
+
+    public function stack(){
+        return $this->belongsTo('Stack', 'id', 'stack_id');
+    }
+
+    public function section(){
+        return $this->hasMany('Section', 'section_id', 'id');
+    }
+
+}
