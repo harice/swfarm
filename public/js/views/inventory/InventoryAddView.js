4c0262f9097b3be9816a3e49e725b9958637f515
William Go <william.go@elementzinteractive.com>
2014-07-15 16:11:48 +0800

    test save inventory

--- public/js/views/inventory/InventoryAddView.js
+++ public/js/views/inventory/InventoryAddView.js
@@ -4,9 +4,11 @@ define([
 	'views/base/AppView',
 	'jqueryvalidate',
 	'jquerytextformatter',
-	'models/trucker/TruckerModel',
+	'models/inventory/InventoryModel',
 	'collections/account/AccountCollection',
 	'collections/product/ProductCollection',
+	'collections/stack/LocationCollection',
+	'collections/inventory/LocationTransactionTypeCollection',
 	'text!templates/layout/contentTemplate.html',
 	'text!templates/inventory/inventoryAddTemplate.html',
 	'text!templates/inventory/inventoryProductItemTemplate.html',
@@ -17,9 +19,11 @@ define([
 			AppView,
 			Validate,
 			TextFormatter,
-			TruckerModel,
+			InventoryModel,
 			AccountCollection,
 			ProductCollection,
+			LocationCollection,
+			LocationTransactionTypeCollection,
 			contentTemplate,
 			inventoryAddTemplate,
 			inventoryProductItemTemplate,
@@ -40,11 +44,11 @@ define([
 			this.options = {
 				productFieldClone: null,
 				productFieldCounter: 0,
-				productFieldClass: ['product_id', 'stacknumber', 'location_from', 'location_to', 'tons', 'unitprice', 'id'],
-				productFieldClassRequired: ['product_id', 'stacknumber', 'tons', 'unitprice'],
+				productFieldClass: ['product_id', 'stacknumber', 'sectionfrom_id', 'sectionto_id', 'tons', 'price', 'id'],
+				productFieldClassRequired: ['product_id', 'stacknumber', 'sectionfrom_id', 'sectionto_id', 'tons', 'price'],
 				productFieldExempt: [],
 				productFieldSeparator: '.',
-				removeComma: ['unitprice', 'tons'],
+				removeComma: ['tons', 'price'],
 			};
 			
 			this.productCollection = new ProductCollection();
@@ -66,10 +70,28 @@ define([
 			this.truckerAccountCollection.on('error', function(collection, response, options) {
 				this.off('error');
 			});
+			
+			this.locationCollection = new LocationCollection();
+			this.locationCollection.on('sync', function() {
+				thisObj.truckerAccountCollection.getTrailerAccounts();
+				this.off('sync');
+			});
+			this.locationCollection.on('error', function(collection, response, options) {
+				this.off('error');
+			});
+			
+			this.locationTransactionTypeCollection = new LocationTransactionTypeCollection();
+			this.locationTransactionTypeCollection.on('sync', function() {
+				thisObj.locationCollection.getModels();
+				this.off('sync');
+			});
+			this.locationTransactionTypeCollection.on('error', function(collection, response, options) {
+				this.off('error');
+			});
 		},
 		
 		render: function(){
-			this.truckerAccountCollection.getTrailerAccounts();
+			this.locationTransactionTypeCollection.getModels();
 			Backbone.View.prototype.refreshTitle(this.h1Title,this.h1Small);
 		},
 		
@@ -91,7 +113,7 @@ define([
 			var compiledTemplate = _.template(contentTemplate, variables);
 			this.subContainer.html(compiledTemplate);
 			
-			this.generateTruckerAccount();
+			this.generateTransactionType();
 			this.focusOnFirstField();
 			this.initValidateForm();
 			this.addProduct();
@@ -102,14 +124,15 @@ define([
 		initValidateForm: function () {
 			var thisObj = this;
 			
-			var validate = $('#trailerForm').validate({
+			var validate = $('#inventoryForm').validate({
 				submitHandler: function(form) {
-					var data = $(form).serializeObject();
-					//data['rate'] = '0.00';
-					//console.log(data);
-					var truckerModel = new TruckerModel(data);
+					var data = thisObj.formatFormField($(form).serializeObject());
 					
-					truckerModel.save(
+					console.log(data);
+					
+					var inventoryModel = new InventoryModel(data);
+					
+					inventoryModel.save(
 						null, 
 						{
 							success: function (model, response, options) {
@@ -122,7 +145,7 @@ define([
 								else
 									thisObj.displayMessage(response);
 							},
-							headers: truckerModel.getAuth(),
+							headers: inventoryModel.getAuth(),
 						}
 					);
 				},
@@ -135,6 +158,7 @@ define([
 			if(this.options.productFieldClone == null) {
 				var productTemplateVars = {
 					product_list:this.getProductDropdown(),
+					location_list:this.getLocationDropdown(),
 				};
 				var productTemplate = _.template(inventoryProductItemTemplate, productTemplateVars);
 				
@@ -150,7 +174,7 @@ define([
 				this.$el.find('#product-list tbody').append(clone);
 			}
 				
-			this.addValidationToProduct();
+			this.addValidationToProduct(clone);
 			return clone;
 		},
 		
@@ -158,6 +182,15 @@ define([
 			var dropDown = '<option value="">Select a product</option>';
 			_.each(this.productCollection.models, function (model) {
 				dropDown += '<option value="'+model.get('id')+'">'+model.get('name')+'</option>';
+			});
+			
+			return dropDown;
+		},
+		
+		getLocationDropdown: function () {
+			var dropDown = '<option value="">Select a location</option>';
+			_.each(this.locationCollection.models, function (model) {
+				dropDown += '<option value="'+model.get('id')+'">'+model.get('locationName')+'</option>';
 			});
 			
 			return dropDown;
@@ -191,13 +224,53 @@ define([
 			}
 		},
 		
-		generateTruckerAccount: function () {
+		generateTransactionType: function () {
 			var options = '';
-			_.each(this.truckerAccountCollection.models, function (model) {
-				options += '<option value="'+model.get('id')+'">'+model.get('name')+'</option>';
+			_.each(this.locationTransactionTypeCollection.models, function (model) {
+				options += '<option value="'+model.get('id')+'">'+model.get('type')+'</option>';
 			});
 			
-			this.$el.find('#account_id').append(options);
+			this.$el.find('#transactiontype_id').append(options);
+		},
+		
+		formatFormField: function (data) {
+			var formData = {products:[]};
+			var productFieldClass = this.options.productFieldClass;
+			
+			for(var key in data) {
+				if(typeof data[key] !== 'function'){
+					var value = data[key];
+					var arrayKey = key.split(this.options.productFieldSeparator);
+					
+					if(arrayKey.length < 2)
+						if(this.options.removeComma.indexOf(key) < 0)
+							formData[key] = value;
+						else
+							formData[key] = this.removeCommaFromNumber(value);
+					else {
+						if(arrayKey[0] == productFieldClass[0]) {
+							var index = arrayKey[1];
+							var arrayProductFields = {};
+							
+							for(var i = 0; i < productFieldClass.length; i++) {
+								if(this.options.productFieldExempt.indexOf(productFieldClass[i]) < 0) {
+									var fieldValue = data[productFieldClass[i]+this.options.productFieldSeparator+index];
+									if(!(productFieldClass[i] == 'id' && fieldValue == '')) {
+										if(this.options.removeComma.indexOf(productFieldClass[i]) < 0)
+											arrayProductFields[productFieldClass[i]] = fieldValue;
+										else
+											arrayProductFields[productFieldClass[i]] = this.removeCommaFromNumber(fieldValue);
+									}
+								}
+							}
+								
+							formData.products.push(arrayProductFields);
+						}
+					}
+				}
+			}
+			
+			return formData;
 		},
 		
 		events: {
