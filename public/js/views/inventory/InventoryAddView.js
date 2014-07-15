e82b452d98d1a4e80d2750235f93a2f453fe5e1e
William Go <william.go@elementzinteractive.com>
2014-07-15 14:16:38 +0800

    inventory list and add client side

--- public/js/views/inventory/InventoryAddView.js
+++ public/js/views/inventory/InventoryAddView.js
@@ -6,8 +6,10 @@ define([
 	'jquerytextformatter',
 	'models/trucker/TruckerModel',
 	'collections/account/AccountCollection',
+	'collections/product/ProductCollection',
 	'text!templates/layout/contentTemplate.html',
-	'text!templates/trucker/inventoryAddTemplate.html',
+	'text!templates/inventory/inventoryAddTemplate.html',
+	'text!templates/inventory/inventoryProductItemTemplate.html',
 	'global',
 	'constant',
 ], function(Backbone,
@@ -17,8 +19,10 @@ define([
 			TextFormatter,
 			TruckerModel,
 			AccountCollection,
+			ProductCollection,
 			contentTemplate,
 			inventoryAddTemplate,
+			inventoryProductItemTemplate,
 			Global,
 			Const
 ){
@@ -33,10 +37,30 @@ define([
 			this.h1Title = 'Inventory';
 			this.h1Small = 'add';
 			
-			this.truckerAccountCollection = new AccountCollection();
-			this.truckerAccountCollection.on('sync', function() {
+			this.options = {
+				productFieldClone: null,
+				productFieldCounter: 0,
+				productFieldClass: ['product_id', 'stacknumber', 'location_from', 'location_to', 'tons', 'unitprice', 'id'],
+				productFieldClassRequired: ['product_id', 'stacknumber', 'tons', 'unitprice'],
+				productFieldExempt: [],
+				productFieldSeparator: '.',
+				removeComma: ['unitprice', 'tons'],
+			};
+			
+			this.productCollection = new ProductCollection();
+			this.productCollection.on('sync', function() {
 				if(thisObj.subContainerExist())
 					thisObj.displayForm();
+				
+				this.off('sync');
+			});
+			this.productCollection.on('error', function(collection, response, options) {
+				this.off('error');
+			});
+			
+			this.truckerAccountCollection = new AccountCollection();
+			this.truckerAccountCollection.on('sync', function() {
+				thisObj.productCollection.getAllModel();
 				this.off('sync');
 			});
 			this.truckerAccountCollection.on('error', function(collection, response, options) {
@@ -70,6 +94,7 @@ define([
 			this.generateTruckerAccount();
 			this.focusOnFirstField();
 			this.initValidateForm();
+			this.addProduct();
 			
 			this.otherInitializations();
 		},
@@ -104,6 +129,68 @@ define([
 			});
 		},
 		
+		addProduct: function () {
+			var clone = null;
+			
+			if(this.options.productFieldClone == null) {
+				var productTemplateVars = {
+					product_list:this.getProductDropdown(),
+				};
+				var productTemplate = _.template(inventoryProductItemTemplate, productTemplateVars);
+				
+				this.$el.find('#product-list tbody').append(productTemplate);
+				var productItem = this.$el.find('#product-list tbody').find('.product-item:first-child');
+				this.options.productFieldClone = productItem.clone();
+				this.addIndexToProductFields(productItem);
+				clone = productItem;
+			}
+			else {
+				var clone = this.options.productFieldClone.clone();
+				this.addIndexToProductFields(clone);
+				this.$el.find('#product-list tbody').append(clone);
+			}
+				
+			this.addValidationToProduct();
+			return clone;
+		},
+		
+		getProductDropdown: function () {
+			var dropDown = '<option value="">Select a product</option>';
+			_.each(this.productCollection.models, function (model) {
+				dropDown += '<option value="'+model.get('id')+'">'+model.get('name')+'</option>';
+			});
+			
+			return dropDown;
+		},
+		
+		addIndexToProductFields: function (bidProductItem) {
+			var productFieldClass = this.options.productFieldClass;
+			for(var i=0; i < productFieldClass.length; i++) {
+				var field = bidProductItem.find('.'+productFieldClass[i]);
+				var name = field.attr('name');
+				field.attr('name', name + this.options.productFieldSeparator + this.options.productFieldCounter);
+			}
+			
+			this.options.productFieldCounter++;
+		},
+		
+		addValidationToProduct: function (clone) {
+			var thisObj = this;
+			var productFieldClassRequired = this.options.productFieldClassRequired;
+			for(var i=0; i < productFieldClassRequired.length; i++) {
+				var rules = {};
+				
+				if(productFieldClassRequired[i] == 'rfv')
+					rules = {require_rfv: true};
+				else
+					rules = {required: true};
+				
+				clone.find('.'+productFieldClassRequired[i]).each(function() {
+					$(this).rules('add', rules);
+				});
+			}
+		},
+		
 		generateTruckerAccount: function () {
 			var options = '';
 			_.each(this.truckerAccountCollection.models, function (model) {
@@ -114,9 +201,22 @@ define([
 		},
 		
 		events: {
+			'click #add-product': 'addProduct',
+			'click .remove-product': 'removeProduct',
 			'click #go-to-previous-page': 'goToPreviousPage',
 			'click #delete-trucker': 'showConfirmationWindow',
 			'click #confirm-delete-trucker': 'deleteTrucker'
+		},
+		
+		removeProduct: function (ev) {
+			$(ev.target).closest('tr').remove();
+			
+			if(!this.hasProduct())
+				this.addProduct();
+		},
+		
+		hasProduct: function () {
+			return (this.$el.find('#product-list tbody .product-item').length)? true : false;
 		},
 		
 		deleteTrucker: function () {
