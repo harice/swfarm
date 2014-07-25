4c1b52031eb435f13d054ff87963a16e48950614
dasigr <romualdo.dasig@elementzinteractive.com>
2014-07-25 13:36:29 +0800

    Require Contract on Reserved SO.

--- app/repositories/OrderRepository.php
+++ app/repositories/OrderRepository.php
@@ -245,6 +245,10 @@ class OrderRepository implements OrderRepositoryInterface {
             $data['status_id'] = 4; //Pending status
         else
             $data['status_id'] = 1; //Open status
+        
+        if ($data['natureofsale_id'] != Config::get('constants.NOS_RESERVED')) {
+            unset($data['contract_id']);
+        }
 
         $this->validate($data, 'Order', $data['ordertype']);
 
@@ -405,7 +409,7 @@ class OrderRepository implements OrderRepositoryInterface {
     }
     
     public function validate($data, $entity, $orderType = null)
-    {   
+    {
         $messages = array(
             'rfv.required_if' => 'The RFV field is required when product is on hold.',
         );
@@ -416,6 +420,10 @@ class OrderRepository implements OrderRepositoryInterface {
                 $validator = Validator::make($data, $entity::$po_rules, $messages);
             } else { //SO rules need to used
                 $validator = Validator::make($data, $entity::$so_rules, $messages);
+                $validator->sometimes('contract_id', 'required', function($data)
+                {
+                    return $data->natureofsale_id == Config::get('constants.NOS_RESERVED');
+                });
             }
         }
         
