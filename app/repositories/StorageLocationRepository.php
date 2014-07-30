b6f346418d546043c5165ee026f697369a5cf03a
dasigr <romualdo.dasig@elementzinteractive.com>
2014-07-30 16:04:42 +0800

    Modify validation message for duplicate section name.

--- app/repositories/StorageLocationRepository.php
+++ app/repositories/StorageLocationRepository.php
@@ -105,7 +105,7 @@ class StorageLocationRepository implements StorageLocationRepositoryInterface {
             DB::rollback();
             $response = array(
             'error' => true,
-            'message' => "A problem was encountered while saving sections.");
+            'message' => "Please enter a unique Section name.");
         }
 
         return $response;
@@ -114,24 +114,50 @@ class StorageLocationRepository implements StorageLocationRepositoryInterface {
 
     private function addSection($storagelocation_id, $sections = array())
     {
+        $response = false;
         foreach ($sections as $section) {
             $section['storagelocation_id'] = $storagelocation_id;
             
             
             if(isset($section['id'])) {
-                $this->validate($section, 'Section', $section['id']);
+                if (!$this->validateSection($section, 'Section', $section['id'])) {
+                    return false;
+                }
                 $sectionLocation = Section::find($section['id']);
             }
             else {
-                $this->validate($section, 'Section');
+                if (!$this->validateSection($section, 'Section')) {
+                    return false;
+                }
                 $sectionLocation = new Section();
             }
 
             $sectionLocation->fill($section);
             $sectionLocation->save();
-
+            
+            $response = true;
         }
-        return true;
+        
+        return $response;
+    }
+    
+    public function validateSection($data, $entity, $id = null)
+    {
+        $messages = array(
+            'name.unique' => 'Section name should be unique.'
+        );
+        
+        if ($id) {
+            $entity::$rules['name'] = 'required|unique:section,name,' .$id;
+        }
+        
+        $validator = Validator::make($data, $entity::$rules, $messages);
+        
+        if($validator->passes()) {
+            return true;
+        }
+        
+        return false;
     }
 
     private function removeSection($storagelocation_id, $sections = array())
@@ -196,7 +222,7 @@ class StorageLocationRepository implements StorageLocationRepositoryInterface {
                 DB::rollback();
                 return Response::json(array(
                     'error' => true,
-                    'message' => "A problem was encountered while saving section"), 500);
+                    'message' => "Please enter a unique Section name."), 500);
             }
         } else {
             DB::rollback();
@@ -223,25 +249,6 @@ class StorageLocationRepository implements StorageLocationRepositoryInterface {
         }
         
     }
-
-    // public function validate($data, $id = null)
-    // {
-    //     $rules = StorageLocation::$rules;
-
-    //     // $rules = array(
-    //     //     'account_id' => 'required',
-    //     //     'name' => 'required|unique:storagelocation,name,'.$data['account_id'].',account_id',
-    //     //     'description' => 'max:250'
-    //     // );
-
-    //     $validator = Validator::make($data, $rules);
-        
-    //     if ($validator->fails()) {
-    //         throw new ValidationException($validator);
-    //     }
-        
-    //     return true;
-    // }
 
     public function validate($data, $entity, $id = null)
     {
