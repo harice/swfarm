f8a5d6bbd0ca0ec50058a6b55efd51f7e112bced
dasigr <romualdo.dasig@elementzinteractive.com>
2014-08-13 15:59:08 +0800

    Update for Product Statement Report

--- app/repositories/ReportRepository.php
+++ app/repositories/ReportRepository.php
@@ -1,6 +1,13 @@
 <?php
 
+use \StorageLocationRepositoryInterface;
+
 class ReportRepository implements ReportRepositoryInterface {
+    
+    public function __construct(StorageLocationRepositoryInterface $storagelocation)
+    {
+        $this->storagelocation = $storagelocation;
+    }
 
     /**
      * Generate a Customer Sales Report
@@ -78,45 +85,111 @@ class ReportRepository implements ReportRepositoryInterface {
     public function generateProducerStatement($params)
     {
         $perPage = isset($params['perpage']) ? $params['perpage'] : Config::get('constants.GLOBAL_PER_LIST');
-        $sortby = isset($params['sortby']) ? $params['sortby'] : 'order.created_at';
+        $sortby = isset($params['sortby']) ? $params['sortby'] : 'weightticket.created_at';
         $orderby = isset($params['orderby']) ? $params['orderby'] : 'dsc';
         
-        $report = WeightTicketProducts::join('weightticketscale', 'weightticketproducts.weightTicketScale_id', '=', 'weightticketscale.id')
-            ->join('transportscheduleproduct', 'weightticketproducts.transportScheduleProduct_id', '=', 'transportscheduleproduct.id')
-            ->join('productorder', 'transportscheduleproduct.productorder_id', '=', 'productorder.id')
-            ->join('products', 'productorder.product_id', '=', 'products.id')
-            ->join('transportschedule', 'transportscheduleproduct.transportschedule_id', '=', 'transportschedule.id')
-            ->join('weightticket', 'transportschedule.id', '=', 'weightticket.transportSchedule_id')
-            ->join('order', 'transportschedule.order_id', '=', 'order.id');
-//            ->join('stack', 'productorder.stacknumber', '=', 'stack.stacknumber')
-//            ->leftJoin('stacklocation', 'stack.id', '=', 'stacklocation.stack_id')
-//            ->leftJoin('section', 'stacklocation.section_id', '=', 'section.id')
-//            ->leftJoin('storagelocation', 'section.storagelocation_id', '=', 'storagelocation.id');
+        $productorder = ProductOrder::with('order.account.address', 'transportscheduleproduct.weightticketproducts.weightticketscale');
         
-        $report = $report
-            ->select(
-                'weightticketproducts.id as id',
-                'weightticketproducts.weightTicketScale_id as weightticketscale_id',
-                'weightticketproducts.transportScheduleProduct_id as transportscheduleproduct_id',
-                'weightticketproducts.bales as bales',
-                'weightticketproducts.pounds as pounds',
-                'transportscheduleproduct.transportschedule_id as transportschedule_id',
-                'transportscheduleproduct.productorder_id as productorder_id',
-                'transportscheduleproduct.sectionto_id as section_id',
-                'transportschedule.order_id as order_id',
-                'productorder.unitprice as unitprice',
-                'products.name as product_name',
-                'weightticket.weightTicketNumber as wtn',
-                'order.order_number as order_number',
-                'order.status_id as order_status_id',
-                'order.created_at as created_at'
-//                'stack.stacknumber as stack_number',
-//                'storagelocation.name as storagelocation_name'
-            )
-            ->orderBy($sortby, $orderby)
-            ->paginate($perPage)
-            ->toArray();
+        if (isset($params['accountId'])) {
+            $productorder = $productorder->whereHas('order', function($q) use ($params)
+            {
+                $q->where('account_id', '=', $params['accountId']);
+            });
+        }
         
-        return $report;
+        $productorder = $productorder->whereHas('transportscheduleproduct', function($q)
+        {
+            $q->with('weightticketproducts');
+            $q->whereHas('weightticketproducts', function($r)
+            {
+                $r->with('product');
+            });
+        });
+        
+        $productorder = $productorder->paginate($perPage);
+        
+        return $productorder;
+        
+//        $report = WeightTicketProducts::join('transportscheduleproduct', 'weightticketproducts.transportScheduleProduct_id', '=', 'transportscheduleproduct.id')
+//            
+////            ->join('productorder', 'transportscheduleproduct.productorder_id', '=', 'productorder.id')
+//            
+//            ->join('transportschedule', 'transportscheduleproduct.transportschedule_id', '=', 'transportschedule.id')
+//            ->join('weightticket', 'transportschedule.id', '=', 'weightticket.transportSchedule_id')
+//            
+//            ->join('productorder', 'transportscheduleproduct.productorder_id', '=', 'productorder.id')
+//            ->join('products', 'productorder.product_id', '=', 'products.id')
+//            ->join('order', 'productorder.order_id', '=', 'order.id')
+//            
+//            ->join('account', 'order.account_id', '=', 'account.id')
+//            ->join('stack', 'productorder.stacknumber', '=', 'stack.stacknumber');
+//        
+//        if (isset($params['dateStart']) && isset($params['dateEnd'])) {
+//            $report = $report->whereBetween('weightticket.created_at', array($params['dateStart'], $params['dateEnd']));
+//        }
+//
+//        if (isset($params['accountId'])) {
+//            $report = $report->where('order.account_id', '=', $params['accountId']);
+//        }
+//        
+//        $report = $report->select(
+//                'weightticketproducts.id as id',
+//                'weightticketproducts.weightTicketScale_id as weightticketscale_id',
+//                'weightticketproducts.transportScheduleProduct_id as transportscheduleproduct_id',
+//                'weightticketproducts.bales as bales',
+//                'weightticketproducts.pounds as pounds',
+//                'transportscheduleproduct.transportschedule_id as transportschedule_id',
+//                'transportscheduleproduct.productorder_id as productorder_id',
+////                'transportscheduleproduct.sectionto_id as section_id',
+//                'transportschedule.order_id as order_id',
+//                'productorder.unitprice as unitprice',
+//                'products.name as product_name',
+//                'weightticket.weightTicketNumber as wtn',
+//                'weightticket.created_at as created_at',
+//                'order.order_number as order_number',
+//                'order.status_id as order_status_id',
+//                'order.account_id as account_id',
+//                'account.name as account_name',
+//                'stack.id as stack_id',
+//                'stack.stacknumber as stack_number'
+//            )
+//            ->orderBy($sortby, $orderby)
+////            ->paginate($perPage)
+//            ->get();
+//        $weightticket_products = $report->toArray();
+//        
+//        foreach ($weightticket_products as &$weightticket_product) {
+//            $weightticket_product['storagelocation_name'] = $this->getLocationName($weightticket_product['account_id'], $weightticket_product['stack_id']);
+//        }
+//        
+////        $result['weightticket_products'] = $weightticket_products;
+////        $result['storagelocations'] = $storagelocations;
+//        
+//        return $weightticket_products;
+    }
+    
+    /**
+     * Get Storage Location Name
+     * 
+     * @param int $account_id
+     * @return string
+     */
+    public function getLocationName($account_id, $stack_id)
+    {
+        $stacklocation = StackLocation::where('stack_id', '=', $stack_id)->first();
+        Log::debug($stacklocation);
+        
+        $params = array(
+            'accountId' => 21
+        );
+        $storagelocations = $this->storagelocation->findAll($params)->toArray();
+        
+        foreach ($storagelocations['data'] as $storagelocation) {
+            foreach ($storagelocation['section'] as $section) {
+                
+            }
+        }
+        
+        return 'Location Name';
     }
 }
