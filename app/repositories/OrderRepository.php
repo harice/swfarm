c6b6223d00b362963e0c6230977d9fb2e1e10893
Roxan Guingao <roxan.guingao@gmail.com>
2014-08-19 18:40:04 +0800

    remove test dump

--- app/repositories/OrderRepository.php
+++ app/repositories/OrderRepository.php
@@ -287,7 +287,7 @@ class OrderRepository implements OrderRepositoryInterface {
     
     public function addOrder($data, $orderType = 1)
     {
-        var_dump($data);exit;
+        // var_dump($data);exit;
         
         $now = new DateTime('NOW');
         $date = $now->format('Y-m-d H:i:s');
