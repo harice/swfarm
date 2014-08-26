d2f8df47431984930eeb3dafefbe67f5dd7bd7d3
Roxan Guingao <roxan.guingao@gmail.com>
2014-08-26 10:03:33 +0800

    debug on

--- app/repositories/DownloadRepository.php
+++ app/repositories/DownloadRepository.php
@@ -5,10 +5,10 @@ class DownloadRepository implements DownloadInterface
 	public function download($params)
 	{
 		if(is_array($params) && array_key_exists('q', $params)) {
-			// var_dump(Request::header());
-			// var_dump(Cookie::get('laravel_session'));
-			// var_dump(Session::all());
-			// exit();
+			var_dump(Request::header());
+			var_dump(Cookie::get('laravel_session'));
+			var_dump(Session::all());
+			exit();
 			$q = unserialize(base64_decode($params['q']));
 			switch ($q['m']) {
 				case 'doc':
@@ -28,7 +28,7 @@ class DownloadRepository implements DownloadInterface
 					break;
 			}
 		}
-		
+		// http://swfarm.local/file?q=YTozOntzOjE6ImkiO3M6MToiMSI7czoxOiJ0IjtzOjE6InYiO3M6MToibSI7czozOiJkb2MiO30=
 		// return App::abort(501,'Not implemented');
 		return Response::view('errors.404', array(), 404);
 	}
