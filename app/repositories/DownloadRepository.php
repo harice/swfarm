94bfe0bb71e0105d722ffc17346fa96e354ca2fd
Roxan Guingao <roxan.guingao@gmail.com>
2014-08-27 16:13:56 +0800

    filter update

--- app/repositories/DownloadRepository.php
+++ app/repositories/DownloadRepository.php
@@ -4,32 +4,26 @@ class DownloadRepository implements DownloadInterface
 {
 	public function download($params)
 	{
-		if(is_array($params) && array_key_exists('q', $params)) {
-			// var_dump(Request::header());
-			// var_dump(Cookie::get('ihYF23kouGY'));
-			// var_dump(Session::all());
-			// exit();
-			$q = unserialize(base64_decode($params['q']));
-			switch ($q['m']) {
-				case 'doc':
-					$file_o = Document::where('issave', '=', 1)->where('id', '=', $q['i'])->first();
-			        if($file_o){
-		        		header('Pragma: public');
-            			header('Expires: 0');
-            			header('Cache-Control: must-revalidate, post-check=0, pre-check=0');
-			        	header('Content-Description: File Transfer');
-			        	header('Content-Transfer-Encoding: binary');
-						
-						// header('Content-Disposition: attachment; filename="downloaded.pdf"');
+		$q = unserialize(base64_decode($params['q']));
+		switch ($q['m']) {
+			case 'doc':
+				$file_o = Document::where('issave', '=', 1)->where('id', '=', $q['i'])->first();
+		        if($file_o){
+	        		header('Pragma: public');
+        			header('Expires: 0');
+        			header('Cache-Control: must-revalidate, post-check=0, pre-check=0');
+		        	header('Content-Description: File Transfer');
+		        	header('Content-Transfer-Encoding: binary');
+					
+					// header('Content-Disposition: attachment; filename="downloaded.pdf"');
 
-			            header('Content-Type: '.$file_o->type);
-			            readfile($file_o->content);
-			        }
-					break;
-			}
+		            header('Content-Type: '.$file_o->type);
+		            readfile($file_o->content);
+		        }
+				break;
 		}
+		
 		// http://swfarm.local/file?q=YTozOntzOjE6ImkiO3M6MToiMSI7czoxOiJ0IjtzOjE6InYiO3M6MToibSI7czozOiJkb2MiO30=
-		// return App::abort(501,'Not implemented');
-		return Response::view('errors.404', array(), 404);
+		return Redirect::to('404')->withPage('file');
 	}
 }
