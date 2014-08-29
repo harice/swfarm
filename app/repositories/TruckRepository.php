d308b6e79db948cc1e69d1a14115699971f4bc45
dasigr <romualdo.dasig@elementzinteractive.com>
2014-08-29 11:39:47 +0800

    Fix displaying all trucks.

--- app/repositories/TruckRepository.php
+++ app/repositories/TruckRepository.php
@@ -14,12 +14,11 @@ class TruckRepository implements TruckRepositoryInterface {
         $sortby = isset($params['sortby']) ? $params['sortby'] : 'trucknumber';
         $orderby = isset($params['orderby']) ? $params['orderby'] : 'asc';
 
-        // return Truck::with('account','account.accounttype')->orderBy($sortby, $orderby)->paginate($perPage);
-
         return Truck::join('account', 'truck.account_id', '=', 'account.id')
-            ->join('account_accounttype', 'account.id','=','account_accounttype.account_id')
-            ->join('accounttype', 'account_accounttype.accounttype_id', '=', 'accounttype.id')
-            ->select('truck.id', 'truck.trucknumber', 'truck.fee', 'account.id as account_id', 'account.name as account_name', 'accounttype.name as account_type')
+//            ->join('account_accounttype', 'account.id','=','account_accounttype.account_id')
+//            ->join('accounttype', 'account_accounttype.accounttype_id', '=', 'accounttype.id')
+//            ->select('truck.id', 'truck.trucknumber', 'truck.fee', 'account.id as account_id', 'account.name as account_name', 'accounttype.name as account_type')
+            ->select('truck.id', 'truck.trucknumber', 'truck.fee', 'account.id as account_id', 'account.name as account_name')
 //            ->groupBy('account.id')
             ->orderBy($sortby, $orderby)
             ->paginate($perPage);
