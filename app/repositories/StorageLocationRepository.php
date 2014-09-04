f49c8c62bc6cd0cb5e746d21b0737dd3a7741a02
Avs Ceriola <avelino.ceriola@elementzinteractive.com>
2014-09-04 10:05:23 +0800

    bug fix

--- app/repositories/StorageLocationRepository.php
+++ app/repositories/StorageLocationRepository.php
@@ -353,7 +353,7 @@ class StorageLocationRepository implements StorageLocationRepositoryInterface {
         return new StorageLocation($data);
     }
 
-    public function getAllStorageLocation(){
+    /*public function getAllStorageLocation(){
         $storageLocation = Section::with('storagelocationName')->get();
         // $storageLocation = StorageLocation::with('section')->get();
         // var_dump($storageLocation->toArray());
@@ -362,6 +362,17 @@ class StorageLocationRepository implements StorageLocationRepositoryInterface {
             array_push($locationResult, array('id' => $location->id, 'locationName' => $location['storagelocation_name']['name'].' - '.$location['name']));
         }
         return $locationResult;
+    }*/
+
+    public function getAllStorageLocation(){
+        $storageLocation = StorageLocation::orderby('name', 'ASC')->get()->toArray();
+        // $storageLocation = StorageLocation::with('section')->get();
+        // var_dump($storageLocation->toArray());
+        // $locationResult = array();
+        // foreach($storageLocation as $location){
+        //     array_push($locationResult, array('id' => $location->id, 'locationName' => $location['storagelocation_name']['name'].' - '.$location['name']));
+        // }
+        return $storageLocation;
     }
     
 }
