85f549b50d3c969c43e2ef01b72ca9ec89080e87
dasigr <romualdo.dasig@elementzinteractive.com>
2014-09-04 15:10:27 +0800

    update Scale

--- public/js/views/scale/ScaleEditView.js
+++ public/js/views/scale/ScaleEditView.js
@@ -25,14 +25,14 @@ define([
 
 	var ScaleEditView = ScaleAddView.extend({
 		el: $("#"+Const.CONTAINER.MAIN),
-		
+
 		initialize: function(option) {
 			this.initSubContainer();
 			var thisObj = this;
 			this.scaleId = option.id;
 			this.h1Title = 'Scale';
 			this.h1Small = 'edit';
-			
+
 			this.scalerAccountCollection = new AccountCollection();
 			this.scalerAccountCollection.on('sync', function() {
 				if(thisObj.subContainerExist()) {
@@ -44,36 +44,36 @@ define([
 			this.scalerAccountCollection.on('error', function(collection, response, options) {
 				this.off('error');
 			});
-			
+
 			this.model = new ScaleModel({id:this.scaleId});
 			this.model.on('change', function() {
 				thisObj.scalerAccountCollection.getScalerAccounts();
 				this.off('change');
 			});
 		},
-		
+
 		otherInitializations: function () {
 			this.initDeleteConfirmation();
 		},
-		
+
 		render: function(){
 			this.model.runFetch();
 			Backbone.View.prototype.refreshTitle('Scale','edit');
 		},
-		
+
 		supplyScaleData: function () {
 			this.$el.find('#account_id').val(this.model.get('account_id'));
 			this.$el.find('#name').val(this.model.get('name'));
 			this.$el.find('#rate').val(this.addCommaToNumber(this.model.get('rate')));
 		},
-		
+
 		initDeleteConfirmation: function () {
-			this.initConfirmationWindow('Are you sure you want to delete this scale?',
+			this.initConfirmationWindow('Are you sure you want to delete this Scale?',
 										'confirm-delete-scale',
 										'Delete');
 		},
 	});
 
 	return ScaleEditView;
-  
+
 });
