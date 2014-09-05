09ba993ef7529036e9f7093c9fca025c054b2728
dasigr <romualdo.dasig@elementzinteractive.com>
2014-09-05 11:10:10 +0800

    minor changes

--- app/models/Truck.php
+++ app/models/Truck.php
@@ -6,7 +6,7 @@
  * @author Avs
  */
 class Truck extends BaseModel {
-    
+
     /**
 	 * The database table used by the model.
 	 *
@@ -15,29 +15,29 @@ class Truck extends BaseModel {
 	protected $table = 'truck';
 
     protected $softDelete = true;
-    
+
     /**
      * Define fillable attributes in a model.
-     * 
+     *
      * @var array
      */
     protected $fillable = array('account_id', 'trucknumber', 'fee');
 
     /**
      * Define field validation rules.
-     * 
+     *
      * @var array
      */
 	public static $rules = array(
         'account_id' => 'required',
-        'trucknumber' => 'required|alpha_num|unique:truck,trucknumber',
+        'trucknumber' => 'required|unique:truck,trucknumber',
         'fee' => 'required|numeric|min:0|max:100000'
     );
 
     public function account(){
         return $this->hasOne('Account', 'id', 'account_id')->select(array('id', 'name'));
     }
-    
+
     public function transportschedule()
     {
         return $this->hasMany('TransportSchedule', 'truck_id', 'id');
