63528a3ee2a59cdfe4a04704c5a88c986ad26024
dasigr <romualdo.dasig@elementzinteractive.com>
2014-09-16 14:54:01 +0800

    Fix #SWF-1010 and #SWF-1011

--- public/js/views/trucker/TruckerView.js
+++ public/js/views/trucker/TruckerView.js
@@ -9,12 +9,12 @@ define([
 
 	var TruckerView = AppView.extend({
 		el: $("#"+Const.CONTAINER.MAIN),
-		
+
 		initialize: function(option) {
 			this.initSubContainer();
             this.truckerId = option.id;
 			var thisObj = this;
-			
+
 			this.model = new TruckerModel({id:option.id});
 			this.model.on("change", function() {
 				if(thisObj.subContainerExist())
@@ -22,12 +22,12 @@ define([
 				this.off("change");
 			});
 		},
-		
+
 		render: function(){
 			this.model.runFetch();
 			Backbone.View.prototype.refreshTitle('Truckers','view');
 		},
-		
+
 		displayTrucker: function () {
 			var innerTemplateVariables = {
                 _: _,
@@ -35,36 +35,37 @@ define([
 				trucker_url:'#/'+Const.URL.TRUCKER,
 				trucker_edit_url:'#/'+Const.URL.TRUCKER+'/'+Const.CRUD.EDIT
 			};
-            
+
             _.extend(innerTemplateVariables,Backbone.View.prototype.helpers);
-            
+
 			var innerTemplate = _.template(truckerViewTemplate, innerTemplateVariables);
-			
+
 			var variables = {
 				h1_title: this.model.get('name'),
 				sub_content_template: innerTemplate
 			};
 			var compiledTemplate = _.template(contentTemplate, variables);
 			this.subContainer.html(compiledTemplate);
-			
+
 			this.initConfirmationWindow('Are you sure you want to delete this trucker?',
 										'confirm-delete-trucker',
-										'Delete');
+										'Delete',
+                                        'Delete Trucker');
 		},
-		
+
 		events: {
 			'click #go-to-previous-page': 'goToPreviousPage',
             'click #delete-trucker': 'showDeleteConfirmationWindow',
 			'click #confirm-delete-trucker': 'deleteTrucker',
 		},
-        
+
         showDeleteConfirmationWindow: function () {
 			this.showConfirmationWindow();
 		},
-		
+
 		deleteTrucker: function (){
 			var thisObj = this;
-            
+
             this.model.destroy({
                 success: function (model, response, options) {
                     thisObj.displayMessage(response);
@@ -81,5 +82,5 @@ define([
 	});
 
 	return TruckerView;
-  
+
 });
