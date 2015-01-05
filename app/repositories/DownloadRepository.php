d05ed1a309924e754abdcc9576366a174dda48a7
Roxan Guingao <roxan.guingao@gmail.com>
2015-01-05 11:40:45 +0800

    producer report to po based

--- app/repositories/DownloadRepository.php
+++ app/repositories/DownloadRepository.php
@@ -1102,7 +1102,8 @@ class DownloadRepository implements DownloadInterface
 			case Config::get('constants.REPORT_PRODUCER'):
 				if(!$this->filterParams($q,array('filterId'))) { $_error = true; break; }
 
-				$report_o = $this->generateProducerStatement($q);
+				// $report_o = $this->generateProducerStatement($q);
+				$report_o = $this->generateProducerStatementByOrder($q);
 				if(!$report_o) { $_notfound = true; break; }
 				break;
 
@@ -1545,6 +1546,106 @@ class DownloadRepository implements DownloadInterface
 	    return $this->parse($report_a);
 	}
 */
+
+	private function generateProducerStatementByOrder($_params = array()) {
+		$_dateBetween = $this->generateBetweenDates($_params);
+		$report_o = Account::with('businessaddress.state')
+                        ->with(array('order' => function($query) use($_dateBetween) {
+                            $query->join('location','location.id','=','order.location_id')
+                            	->join('productorder','productorder.order_id','=','order.id')
+                                ->join('transportscheduleproduct','transportscheduleproduct.productorder_id','=','productorder.id')
+                                ->join('transportschedule','transportschedule.id','=','transportscheduleproduct.transportschedule_id')
+                                ->join('weightticket','weightticket.transportschedule_id','=','transportschedule.id')
+                                ->where('order.ordertype','=',Config::get('constants.ORDERTYPE_PO'))
+                                ->where('transportschedule.status_id','=',Config::get('constants.STATUS_CLOSED'))
+                                ->where('weightticket.status_id','=',Config::get('constants.STATUS_CLOSED'))
+                                ->whereBetween('weightticket.updated_at',array_values($_dateBetween))
+                                ->orderBy('order.created_at','DESC')
+                                ->groupBy('order.id')
+                                ->select(array(
+                                            'order.id', 
+                                            'account_id', 
+                                            'order_number',
+                                            'location.location'
+                                        ));
+                        }))
+                        ->where('id','=',$_params['filterId'])
+                        ->first();
+    	
+	    if(!$report_o) return false;
+
+	    $report_a = $report_o->toArray();
+	    $report_a['report_date'] = $_dateBetween;
+	    $report_a['amount'] = 0.00;
+	    $amount_i = 0.00;
+	    
+	    if($report_o->order->count() == 0) return $this->parse($report_a);
+
+	    foreach ($report_a['order'] as $order_key => $order_a) {
+	    	$product_a = ProductOrderSummary::join('products','products.id','=','productordersummary.product_id')
+	    					->with(array('productorder.transportscheduleproduct.transportschedule.weightticket' => function($query) use($_dateBetween) {
+	    						$query->where('weightticket.status_id','=',Config::get('constants.STATUS_CLOSED'))
+	                                ->whereBetween('weightticket.updated_at',array_values($_dateBetween))
+	                                ->whereNotNull('weightticket.pickup_id')
+	                                ->select(array('weightticket.id as wid','transportSchedule_id','weightTicketNumber','updated_at','pickup_id'));
+	    					}))
+                            ->where('order_id','=',$order_a['id'])
+                            ->orderBy('products.name','ASC')
+                            ->select(array('productordersummary.id','products.name as product','unitprice'))
+                            ->get()
+                            ->each(function($productordersummary){
+                            	$productordersummary->productorder->each(function($productorder){ 
+                            		$productorder->transportscheduleproduct->each(function($transportscheduleproduct) use($productorder) {
+                            			$weightticketproduct_o = WeightTicketProducts::join('transportscheduleproduct','transportscheduleproduct.id','=','weightticketproducts.transportscheduleproduct_id')
+                            										->where('weightticketproducts.transportscheduleproduct_id','=',$transportscheduleproduct->id)
+                            										->where('weightticketproducts.weightticketscale_id','=',$transportscheduleproduct->transportschedule->weightticket->pickup_id)
+                            										->where('transportscheduleproduct.productorder_id','=',$productorder->id)
+                            										->select(array('bales','pounds'))
+                            										->first();
+
+										$transportscheduleproduct->bales = $weightticketproduct_o->bales;
+										$transportscheduleproduct->pounds = $weightticketproduct_o->pounds;
+										$transportscheduleproduct->tons = $weightticketproduct_o->tons;
+										$transportscheduleproduct->weightTicketNumber = $transportscheduleproduct->transportschedule->weightticket->weightTicketNumber;
+										$transportscheduleproduct->updated_at = $transportscheduleproduct->transportschedule->weightticket->updated_at;
+										$transportscheduleproduct->wid = $transportscheduleproduct->transportschedule->weightticket->wid;
+
+										$transportscheduleproduct->setVisible(array('bales','tons','pounds','weightTicketNumber','updated_at','wid'));
+                            		});
+
+                            		$productorder->setVisible(array('id','stacknumber','transportscheduleproduct'));
+                            	});
+                            })
+                            ->toArray();
+
+            foreach ($product_a as $product_key => $product) {
+	            $productorder_a = array();
+	            $_tons = 0.0000;
+	            foreach ($product['productorder'] as $productorder_key => $productorder) {
+	            	foreach ($productorder['transportscheduleproduct'] as $key => $weightticket) {
+		                if(array_key_exists($weightticket['wid'], $productorder_a)) {
+		                    $productorder_a[$weightticket['wid']]['bales'] = bcadd($productorder_a[$weightticket['wid']]['bales'],$weightticket['bales'],0);
+		                    $productorder_a[$weightticket['wid']]['pounds'] = bcadd($productorder_a[$weightticket['wid']]['pounds'],$weightticket['pounds'],2);
+		                    $productorder_a[$weightticket['wid']]['tons'] = bcadd($productorder_a[$weightticket['wid']]['tons'],$weightticket['tons'],4);
+	                    	$_tons = bcadd($_tons,$weightticket['tons'],4);
+		                } else {
+		                    $productorder_a[$weightticket['wid']] = $weightticket;
+		                    $_tons += $weightticket['tons'];
+		                }
+		            }
+	            }
+	            $product_a[$product_key]['productorder'] = array_values($productorder_a);
+	            $amount_p = $_tons * $product['unitprice'];
+            	$amount_i += $amount_p; 
+	        }
+
+	        $report_a['order'][$order_key]['productsummary'] = $product_a;
+	    }
+
+	    $report_a['amount'] = $amount_i;
+	    return $this->parse($report_a);
+	}
+
 	private function generateCustomerStatement($_params = array()) {
 		$_dateBetween = $this->generateBetweenDates($_params);
 		$report_o = Account::with('businessaddress.state')
@@ -2140,8 +2241,8 @@ class DownloadRepository implements DownloadInterface
 	}
 
 	private function generateBetweenDates($_params = array()) {
-		$_dates['start'] = array_key_exists('dateStart',$_params) ? date('Y-m-d \0\0\:\0\0\:\0\0', strtotime($_params['dateStart'])) : date('Y-m-d \0\0\:\0\0\:\0\0');
-		$_dates['end'] = array_key_exists('dateEnd',$_params) ? date('Y-m-d \2\3\:\5\9\:\5\9', strtotime($_params['dateEnd'])) : date('Y-m-d \2\3\:\5\9\:\5\9');
+		$_dates['start'] = array_key_exists('dateStart',$_params) ? !empty($_params['dateStart']) ? date('Y-m-d \0\0\:\0\0\:\0\0', strtotime($_params['dateStart'])) : date('Y-m-d \0\0\:\0\0\:\0\0') : date('Y-m-d \0\0\:\0\0\:\0\0');
+		$_dates['end'] = array_key_exists('dateEnd',$_params) ? !empty($_params['dateEnd']) ? date('Y-m-d \2\3\:\5\9\:\5\9', strtotime($_params['dateEnd'])) : date('Y-m-d \2\3\:\5\9\:\5\9') : date('Y-m-d \2\3\:\5\9\:\5\9');
 
 		return $_dates;
 	}
