ef8db168b33258903af2d66fb26720cc81a6ebca
ry_elementz <reizel.yase@elementzinteractive.com>
2015-01-19 10:18:34 +0800

    Reserve Customer

---
+++ public/js/views/reports/ReserveCustomerView.js
@@ -0,0 +1,83 @@
+define([
+	'backbone',
+	'views/base/ReportView',
+	'models/reports/ReportModel',
+	'collections/trucker/TruckerCollection',
+	'text!templates/reports/TruckersPayListTemplate.html',
+	'global',
+	'constant',
+], function(
+	Backbone,
+	ReportView,			
+	Report,
+	TruckerCollection,
+	truckerListTemplate,
+	Global,
+	Const
+){
+
+	var TruckersSearchView = ReportView.extend({
+		
+		initialize: function() {
+			var thisObj = this;			
+			this.filtername = "Truck Name";
+			this.title = "Reserve Customer";
+			this.dataModel = "reserve-customer";					
+
+			this.collection = new TruckerCollection();
+
+			//Only display form when finished synching
+			this.collection.on('sync', function (){																
+				this.off('sync');
+			});
+
+			if(typeof this.otherInits != "undefined")		
+				this.otherInits();						
+
+		},
+		
+		render: function(){	
+			this.getReserveCustomerList();		
+			Backbone.View.prototype.refreshTitle('Report','Reserve Customer');
+		},	
+
+		getReserveCustomerList: function (){	
+			var thisObj = this;						
+			this.collection.fetch({
+				success: function (collection, response, options) {
+					thisObj.displayForm();	
+				},
+				error: function (collection, response, options) {
+				},
+				headers: this.collection.getAuth()
+			});
+		},			
+
+		getFilterName: function (){
+			var truckers = '<option disabled selected>Select Truck</option>';				
+			_.each(this.collection.models, function (model) {	
+				_.each(model.get('data'), function (truck) {
+					truckers += '<option value="'+truck.id+'">'+truck.trucknumber +'</option>';													
+				});						
+			});
+			
+			return truckers;
+		},	
+
+		onclickgenerate: function(data) {
+			var thisObj = this;	
+			
+			this.model = new Report();		
+			this.model.fetchStatement(data['reporttype'], data['filtername'], data['transportdatestart'], data['transportdateend']);										
+			this.model.on('change', function() {
+				thisObj.processData(thisObj.model, truckerListTemplate, data['transportdatestart'], data['transportdateend']);
+				this.off("change");
+			});	
+		},							
+
+		
+	});
+
+  return TruckersSearchView;
+  
+});
