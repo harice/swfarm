39c07f81495c498c0fa765b730a25e5a8f92eeef
avelinoceriola <avelino.ceriola@elementzinteractive.com>
2015-03-04 18:32:43 +0800

    notification module updates

---
+++ app/controllers/APIv1/NotificationController.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace APIv1;
+
+use BaseController;
+use Illuminate\Support\Facades\Response;
+use NotificationRepositoryInterface;
+use Input;
+
+/**
+ * Description of TruckController
+ *
+ * @author Avs
+ */
+
+class NotificationController extends BaseController {
+    
+    public function __construct(NotificationRepositoryInterface $repo)
+    {
+        $this->repo = $repo;
+    }
+
+    public function pullNotification($userId){
+        $model = $this->repo->pullNotification($userId);
+        return Response::json($model);
+    }
+
+    public function getNumberOfNotification($userId){
+        $model = $this->repo->getNumberOfNotification($userId);
+        return Response::json($model);
+    }
+
+}
---
+++ app/database/migrations/2015_03_02_155451_create_notification_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateNotificationTable extends Migration {
+
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+		Schema::create('notification', function(Blueprint $table)
+		{
+			$table->engine = 'InnoDB';
+			$table->increments('id');
+            $table->integer('user_id')->unsigned()->nullable();
+            $table->boolean('isSeen')->default(false);
+			$table->timestamps();
+
+			$table->foreign('user_id')->references('id')->on('users');
+		});
+	}
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		Schema::drop('notification');
+	}
+
+}
