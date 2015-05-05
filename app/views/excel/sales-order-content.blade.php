d8a33f683425a3b5383394a167e9773c0cf31d62
Jonathan Cutura <jonathan.cutura@elementzinteractive.com>
2015-05-05 14:36:40 +0800

    po.so export changes

--- app/views/excel/sales-order-content.blade.php
+++ app/views/excel/sales-order-content.blade.php
@@ -8,77 +8,20 @@
 		<thead>
 			<tr>
 				<th class='width-9' {{$border_s}}> SO No.</th>
-				<th class='width-11' {{$border_s}}> Customer </th>
-				<th class='width-12' {{$border_s}}> Nature of Sale </th>
-				<th class='width-15' {{$border_s}}> Status </th>
-				<th class='width-6' {{$border_s}}> Sales Date </th>
-				<th class='width-8' {{$border_s}}> Delivery  Start </th>
-				<th class='width-6' {{$border_s}}> Delivery  End </th>
-				<th class='width-8' {{$border_s}}> Progress </th>
-				<th class='width-10' {{$border_s}}> Total Amount </th>
+				<th class='width-9' {{$border_s}}> productname </th>
+				<th class='width-9' {{$border_s}}> stacknumber </th>
 			</tr>
 		</thead>
 		@foreach ($data_o as $so)
-		<tr>
-			<td {{$border_s}}>{{ $so->order_number }}</td>
-			<td {{$border_s}}>{{ $so->account->name }}</td>
-			<td {{$border_s}}>@if (!is_null($so->natureofsale_id)) {{ $so->natureofsale->name; }}  @endif </td>
-			<td {{$border_s}}>{{ $so->status->name }}</td>
-			<td {{$border_s}}>{{ strtoupper(date('d M Y',strtotime($so->created_at))) }}</td>
-			<td {{$border_s}}>{{ strtoupper(date('d M Y',strtotime($so->transportdatestart))) }}</td>
-			<td {{$border_s}}>{{ strtoupper(date('d M Y',strtotime($so->transportdateend))) }}</td>
-			<td {{$border_s}}>{{ $so->weightPercentageDelivered }} % </td>
-			<td {{$border_s}}> $ {{ number_format($so->totalPrice,2,'.',',') }}</td>
-		</tr>
-			@foreach ($so->transportscheduleDetails as $schedDetails)
-			<tr>
-				<td></td>
-				<td {{$border_s}}> Stack No: {{ $schedDetails['stackNumber'] }}</td>
-				<td {{$border_s}}> Product: {{ $schedDetails['productName'] }} </td>
-				<td {{$border_s}}></td>
-				<td {{$border_s}}></td>
-			</tr>
-			<thead>
-			<tr>
-				<th></th>
-				<th class='width-9' {{$border_s}} > Schedule </th>
-				<th class='width-11' {{$border_s}}> Weight Ticket No. </th>
-				<th class='width-12' {{$border_s}}> Expected </th>
-				<th class='width-15' {{$border_s}}> Delivered </th>
-			</tr>
-			</thead>
-				@if (sizeof($schedDetails['schedule']) > 0)
-					@foreach ($schedDetails['schedule'] as $sched)
-					<tr>
-						<td></td>
-						<td {{$border_s}}> {{ strtoupper(date('d M Y h:i:s a',strtotime($sched['transportscheduledate']))) }} </td>
-						<td {{$border_s}}> {{ $sched['weightTicketNumber'] }} </td>
-						<td {{$border_s}}> {{ number_format($sched['expected'],3,'.',',')  }}  </td>
-						<td {{$border_s}}> {{ number_format($sched['delivered'],3,'.',',') }} </td>
-					</tr>
-					@endforeach
-					<tr>
-						<td colspan='8' class='text-right' ></td>
-					</tr>
-				@else
-					<tr>
-						<td></td>
-						<td colspan='8' class='text-right' >No transactions found.</td>
-					</tr>
-				@endif
+			@foreach($so->productsummary as $prodsum)
+				@foreach($prodsum['productorder'] as $prodorder)
 				<tr>
-					<td></td>
-					<td class='text-right' >Expected: {{ number_format($schedDetails['totalExpected'],3,'.',',') }}</td>
-					<td class='text-right' >Total Deliveries: {{ number_format($schedDetails['totalDeliveries'],3,'.',',') }}</td>
-					<td class='text-right' >Remaining: {{ number_format($schedDetails['totalExpected'] - $schedDetails['totalDeliveries'],3,'.',',') }}</td>
+					<td {{$border_s}}> {{ $so->order_number }} </td>
+					<td {{$border_s}}> {{ $prodsum['productname']['name'] }} </td>
+					<td {{$border_s}}> {{ $prodorder['stacknumber'] }}</td>
 				</tr>
-				<tr>
-					<td colspan='8' class='text-right' ></td>
-				</tr>
+				@endforeach
 			@endforeach
-			<tr>
-				<td colspan='8' class='text-right' ></td>
-			</tr>
 		@endforeach
 	</table>
 @else
--- app/views/excel/sales-order-header.blade.php
+++ app/views/excel/sales-order-header.blade.php
@@ -1,10 +1,1 @@
-<table>
-	<tr>
-		<td colspan='9' align='center'><b>Sales Order</b></td>
-	</tr>
-	<tr>
-		<td colspan='9' align='center'></td>
-	</tr>
-</table>
-
 {{ $_nest_content }}
